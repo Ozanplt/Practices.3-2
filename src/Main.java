6ae8a51cbc0cf29812e3064cfdfd4c759dbea1f3
Ozanplt <101361859+Ozanplt@users.noreply.github.com>
2022-03-11 01:20:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Instructor instructor1 = new Instructor("XXX","Ali","Veli",123456);
-        Instructor instructor2 = new Instructor("YYY","Duty","Free",78901);
+        Instructor instructor1 = new Instructor("Engin_Demiroğ","Engin","Demiroğ",123456);
+        Student student2 = new Student("YYY","qweqasd","asdasdqw",78901);
         Student student1 = new Student("ZZZ","Ozan","Polat",172391);
 
         InstructorManager instructorManager = new InstructorManager();
@@ -10,7 +10,7 @@ public class Main {
         //operations
         studentManager.add(student1);
         instructorManager.login(instructor1);
-        instructorManager.delete(instructor2);
+        studentManager.delete(student2);
 
     }
 }
